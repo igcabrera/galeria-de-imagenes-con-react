e56c62c0d594ccf513d7bdb9b0e9d7f66c432578
igcabrera <icabrera.illanes@gmail.com>
2022-11-30 20:47:21 -0300

    cambio footer

--- src/new_modules/footer/Footer.jsx
+++ src/new_modules/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import './footer.css'
+import Badge from 'react-bootstrap/Badge';
 
 function Footer() {
     return (
-        <footer class="alert alert-primary" role="alert">
-              Sesión Fotografica de Futbol 2022
+        <footer>
+        <h1>
+            Futbol <Badge bg="info">2022</Badge>
+        </h1>
         </footer>
 
     );
 }
 
 export default Footer;
+
